7641de72764247a506864b9a015e9d0efe27c2b1
arielhernandezcl <ariel.dox@gmail.com>
2024-03-08 20:52:39 -0300

    fix card bootstrap

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,12 +8,12 @@ const userList = document.getElementById("users");
         data.results.forEach(users => { 
             const innerUsers = document.createElement("div");
             innerUsers.classList.add("card", "px-0","m-1", "text-nowrap");
-            innerUsers.style.width = "18rem";
+            innerUsers.style.width = "20rem";
             innerUsers.innerHTML = `
                 <img src="${users.picture.large}" class="card-img-top" alt="User Image">
                 <div class="card-body text-center">
                     <h5 class="card-title">${users.name.first} ${users.name.last}</h5>
-                    <p class="card-text">Email: ${users.email}</p>
+                    <p class="card-text fs-6 ">Email: ${users.email}</p>
                     <p class="card-text">Phone: ${users.phone}</p>
                 </div>`;
             userList.appendChild(innerUsers);

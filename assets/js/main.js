a0ebc6b42de05693edc882cb2a2b8d9506875196
arielhernandezcl <ariel.dox@gmail.com>
2024-03-08 20:36:30 -0300

    add card bootstrap

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const liPost = document.getElementById("users");
+const userList = document.getElementById("users");
 
 (async function() {
     try {
@@ -6,15 +6,18 @@ const liPost = document.getElementById("users");
         const data = await response.json();
         
         data.results.forEach(users => { 
-            const li = document.createElement("p");
-            li.innerHTML = `
-            <img src="${users.picture.large}" alt="">
-            <p>${users.name.first} ${users.name.last}</p>
-            <p>${users.email}</p>
-            <p>${users.phone}</p>
-            `;
-            liPost.appendChild(li);
-            
+            const innerUsers = document.createElement("div");
+            innerUsers.classList.add("card");
+            innerUsers.style.width = "16rem";
+            innerUsers.innerHTML = `
+                <img src="${users.picture.large}" class="card-img-top" alt="User Image">
+                <div class="card-body text-center">
+                    <h5 class="card-title">${users.name.first} ${users.name.last}</h5>
+                    <p class="card-text">Email: ${users.email}</p>
+                    <p class="card-text">Phone: ${users.phone}</p>
+                </div>`;
+            userList.appendChild(innerUsers);
+
         });
     } catch (error) {
         console.log(error);
